0197491b72df6424b36194f5a09cf0aa4792a701
Anton Androsov <aandrosov.primary@gmail.com>
2026-08-08 11:13:51 +0300

    refactor: update freezed generated code, simplify root widget, and downgrade dependencies in pubspec

--- lib/features/notes/data/models/note_model.freezed.dart
+++ lib/features/notes/data/models/note_model.freezed.dart
@@ -116,10 +116,7 @@ return $default(_that);case _:
 final _that = this;
 switch (_that) {
 case _NoteModel():
-return $default(_that);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that);}
 }
 /// A variant of `map` that fallback to returning `null`.
 ///
@@ -178,10 +175,7 @@ return $default(_that.id,_that.title,_that.content,_that.createdAt,_that.updated
 @optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( int id,  String title,  String content,  DateTime createdAt,  DateTime updatedAt)  $default,) {final _that = this;
 switch (_that) {
 case _NoteModel():
-return $default(_that.id,_that.title,_that.content,_that.createdAt,_that.updatedAt);case _:
-  throw StateError('Unexpected subclass');
-
-}
+return $default(_that.id,_that.title,_that.content,_that.createdAt,_that.updatedAt);}
 }
 /// A variant of `when` that fallback to returning `null`
 ///
